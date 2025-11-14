4b3ace1780d667fb35b672dc3c751c3232dc654f
AleksChek26 <check1984@gmail.com>
2025-11-14 12:28:52 +0500

    migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,30 +1,42 @@
 from django.db import models
+from django.db.models import SET_NULL
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
-    category = models.CharField(max_length=150, verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена продукта')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to="images/", verbose_name="Изображение")
+    category = models.ForeignKey(
+        "Category",
+        on_delete=SET_NULL,
+        verbose_name="Категория",
+        null=True,
+        blank=True,
+        related_name="products",
+    )
+    price = models.IntegerField(verbose_name="Цена продукта")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
 
     def __str__(self):
-        return f'{self.name} {self.category}'
+        return f"{self.name} {self.category}"
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name"]
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f"{self.name} {self.description}"
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
